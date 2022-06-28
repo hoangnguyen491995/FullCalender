a0366318953d175eca3b8222ca2baa78b7944bf3
new name <hoangnguyen38017@gmail.com>
2022-06-29 06:55:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import FullCalendar from "@fullcalendar/react"; // must go before plugins
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 
 function App() {
+  
+ const renderEventContent =(eventInfo)=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
+}
+
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin]}
+      
+      eventContent={renderEventContent}
+     
+    />
   );
 }
 
